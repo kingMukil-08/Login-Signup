1c14dde2e8a44d481b14455f3462e0c7a013f97f
kingMukil-08 <130648428+kingMukil-08@users.noreply.github.com>
2023-04-14 16:49:14 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+
+
+import React from 'react';
+import About from './about_page/about';
+
+function App() {
+  return (
+    <div className="App">
+        {/* <Routes>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/signup" element={<SignUp/>}/>
+        </Routes> */}
+        <About></About>
+    </div>
+  );
+}
+
+export default App;
